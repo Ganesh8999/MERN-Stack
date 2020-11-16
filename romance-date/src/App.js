5ee33c029575d842cb1c11b7b1215714750bf8ef
Ganesh8999 <Ganesh8999@github.com>
2020-11-16 13:56:02 +0530

    initial setup of romance app

---
+++ romance-date/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Header from "./Header";
+
+function App() {
+  return (
+    // BEM class naming convention
+    <div className="app">
+      {/* <h1>This is the dating app. </h1> */}
+      <Header />
+      {/* Romance Cards */}
+      {/* SwipeButtons */}
+    </div>
+  );
+}
+
+export default App;
---
+++ romance-date/src/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Header.css";
+import PersonIcon from "@material-ui/icons/Person";
+import IconButton from "@material-ui/core/IconButton";
+
+function Header() {
+  return (
+    <div className="header">
+      {/* <h2>Romance header !! :) </h2> */}
+      <IconButton>
+        <PersonIcon fontSize="large" className="header_icon" />
+      </IconButton>
+      <img
+        className="header_logo"
+        src="https://1000logos.net/wp-content/uploads/2018/07/tinder-logo-768x432.png"
+        alt="romance_logo"
+      />
+    </div>
+  );
+}
+
+export default Header;
