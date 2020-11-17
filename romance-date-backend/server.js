ba863ca6aa72500a2930bb9433db882e7dce68c5
Ganesh8999 <Ganesh8999@github.com>
2020-11-17 21:21:37 +0530

    added app config and API endpoints in server.js

---
+++ romance-date-backend/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import mongoose from "mongoose";
+
+//App Config
+const app = express();
+const port = process.env.port || 8001;
+
+// Middlewares
+
+//DB Config
+
+// API Endpoints
+app.get("/", (req, res) => {
+  res.status(200).send("Hello NodeJs Program");
+});
+
+//Listener
+app.listen(port, () => console.log(`listening on localhost :  ${port}`));
