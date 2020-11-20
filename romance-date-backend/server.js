54c9c5b79c6ba39b09da02a48af80dde7171b159
Ganesh8999 <Ganesh8999@github.com>
2020-11-20 17:15:36 +0530

    created the db connnection

--- romance-date-backend/server.js
+++ romance-date-backend/server.js
@@ -1,13 +1,19 @@
 import express from "express";
-//import mongoose from "mongoose";
+import mongoose from "mongoose";
 
 //App Config
 const app = express();
 const port = process.env.port || 8001;
+const connection_url = `mongodb+srv://admin:s0HLm47uyJezvoAV@romancedatecluster.agwfe.mongodb.net/<romancedatedb>?retryWrites=true&w=majority`;
 
 // Middlewares
 
 //DB Config
+mongoose.connect(connection_url, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
 
 // API Endpoints
 app.get("/", (req, res) => {
