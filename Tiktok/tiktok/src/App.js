53bfd43cf96b733fddd7558b15302ebe166929f3
Ganesh8999 <Ganesh8999@github.com>
2020-11-21 12:14:10 +0530

    added video component

--- Tiktok/tiktok/src/App.js
+++ Tiktok/tiktok/src/App.js
@@ -1,9 +1,19 @@
 import "./App.css";
+import Video from "./Video.js";
 
 function App() {
   return (
     <div className="App">
       <h1>tiktok clone</h1>
+
+      <Video />
+      {/* app container */}
+      {/* videos */}
+      {/* video */}
+      {/* video */}
+      {/* video */}
+      {/* video */}
+      {/* video */}
     </div>
   );
 }
---
+++ Tiktok/tiktok/src/Video.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Video.css";
+
+function Video() {
+  return (
+    <div className="video">
+      <iframe
+        title="11"
+        src="https://m.tiktok.com/v/6621886865379167494.html"
+        scrolling="no"
+      ></iframe>
+
+      {/* Video Footer */}
+      {/* Video Sidebar */}
+    </div>
+  );
+}
+
+export default Video;
