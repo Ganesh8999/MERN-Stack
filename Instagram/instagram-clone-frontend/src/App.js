7de675285a7eaf530c33735d8936be3f2c425764
Ganesh8999 <Ganesh8999@github.com>
2020-11-21 13:47:37 +0530

    instagram clone initial setup

---
+++ Instagram/instagram-clone-frontend/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Instagram clone</h1>
+
+      <div className="app__header"></div>
+      {/* Header */}
+      {/* Posts */}
+      {/* Posts */}
+      {/* Posts */}
+    </div>
+  );
+}
+
+export default App;
